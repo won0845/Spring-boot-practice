ab406dc39d9226eaea75797cd19726d5c0c28ff5
won0845 <tpqms775@gmail.com>
2024-04-11 23:00:06 +0900

    feat: 변수명 통일

--- src/routes/Save.js
+++ src/routes/Save.js
@@ -3,10 +3,10 @@ import React, {useState} from "react";
 import axios from 'axios';
 function Save() {
     const navigate = useNavigate();
-    const [Writer, setWriter] = useState("");
-    const [Pass, setPass] = useState("");
-    const [Title, setTitle] = useState("");
-    const [Content, setContent] = useState("");
+    const [writer, setWriter] = useState("");
+    const [pass, setPass] = useState("");
+    const [title, setTitle] = useState("");
+    const [contents, setContent] = useState("");
 
     // const handleSubmit = (event) => {
     //     event.preventDefault(); // 폼의 기본 동작 중지
@@ -29,13 +29,12 @@ function Save() {
     // };
     const handleSubmit = async (event) => {
         event.preventDefault(); // 폼의 기본 동작 중지
-        console.log(Writer, Pass, Title, Content);
         console.log("버튼 저장 실행")
         await axios.post("/api/board/save",{
-            boardWriter: Writer,
-            boardPass: Pass,
-            boardTitle: Title,
-            boardContent: Content
+            boardWriter: writer,
+            boardPass: pass,
+            boardTitle: title,
+            boardContent: contents
         }).then((res) => {
             console.log(res);
             console.log(res.data);
@@ -53,16 +52,16 @@ function Save() {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                Writer : <input type="text" name="boardWriter" value={Writer} onChange={event => {
+                Writer : <input type="text" name="boardWriter" value={writer} onChange={event => {
                 setWriter(event.target.value)
             }}/> <br/>
-                pw : <input type="password" name="boardPass" value={Pass} onChange={event => {
+                pw : <input type="password" name="boardPass" value={pass} onChange={event => {
                 setPass(event.target.value)
             }}/> <br/>
-                Title : <input type="text" name="boardTitle" value={Title} onChange={event => {
+                Title : <input type="text" name="boardTitle" value={title} onChange={event => {
                 setTitle(event.target.value)
             }}/> <br/>
-                Content : <textarea name="boardContent" cols="30" rows="10" value={Content} onChange={event => {
+                Content : <textarea name="boardContent" cols="30" rows="10" value={contents} onChange={event => {
                 setContent(event.target.value)
             }}></textarea> <br/>
                 <button type="submit" value="Save"/>
