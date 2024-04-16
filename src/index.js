19df54ae4fdd7bd1f453c9d6d79d3b7ea308676b
won0845 <tpqms775@gmail.com>
2024-04-17 02:24:17 +0900

    refactor: 렌더링 두번 되는 에러수정

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <App />
-  </React.StrictMode>
+  //</React.StrictMode> 렌더링 두번 되는 에러?
 );
 
 // If you want to start measuring performance in your app, pass a function
